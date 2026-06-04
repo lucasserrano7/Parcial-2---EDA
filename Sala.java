ea8565e6228941f9c4412e8742e1e3716476ec91
Lucas <serranolucas1000@gmail.com>
2026-06-04 18:09:16 -0300

    Clase salas
    getters, setter, constructos, metodos q pueden estar

--- Sala.java
+++ Sala.java
@@ -1,12 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 /**
  *
  * @author lucas
  */
-public class newClass {
+public class Sala {
+    private int numeroSala;
+    private String descripcionSala;
+    private boolean salida;
+
+    public Sala(int numeroSala, String descripcionSala, boolean salida) {
+        this.numeroSala = numeroSala;
+        this.descripcionSala = descripcionSala;
+        this.salida = salida;
+    }
+    
+    // Getters y Setters
+
+    public int getNumeroSala() {
+        return numeroSala;
+    }
+
+    public void setNumeroSala(int numeroSala) {
+        this.numeroSala = numeroSala;
+    }
+
+    public String getDescripcionSala() {
+        return descripcionSala;
+    }
+
+    public void setDescripcionSala(String descripcionSala) {
+        this.descripcionSala = descripcionSala;
+    }
+
+    public boolean isSalida() {
+        return salida;
+    }
+
+    public void setSalida(boolean salida) {
+        this.salida = salida;
+    }
+    
+    public void agregarConexion(){
+        
+    }
+    public void obtenerSiguente(){
+        
+    }
+    
     
 }
